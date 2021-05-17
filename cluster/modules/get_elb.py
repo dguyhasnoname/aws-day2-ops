6d781c8657a3df846d5f64378f378ccaf0c4395f
dguyhasnoname <dguyhasnoname@gmail.com>
2021-05-17 22:51:14 +0530

    updated elb module

--- cluster/modules/get_elb.py
+++ cluster/modules/get_elb.py
@@ -1,7 +1,7 @@
 
-class _Elb():
+class GetElb():
 
-    def get_elb(session, cluster, verbose):
+    def get_elb(session, cluster):
         elb_client = session.client('elb')
         elb_list = elb_client.describe_load_balancers()
         elb_data, all_elb_data, elb_details = [], [], []
@@ -10,32 +10,32 @@ class _Elb():
             elb_logs, elb_cross_zone, listner = '', '', ''
             elb_name = elb['LoadBalancerName']
             
-            if verbose:
-                elb_attributes = elb_client.describe_load_balancer_attributes(LoadBalancerName=elb_name)
-                try: 
-                    if type(elb_attributes['LoadBalancerAttributes']['CrossZoneLoadBalancing']['Enabled']) == type(True):
-                        elb_cross_zone = u'\u2714'
-                except KeyError:
-                    elb_cross_zone = ''
-                try:
-                    elb_access_logs = elb_attributes['LoadBalancerAttributes']['AccessLog']['Enabled']
-                except KeyError:
-                    elb_access_logs = ''
-                try:
-                    elb_bucket_name = elb_attributes['LoadBalancerAttributes']['AccessLog']['S3BucketName']
-                except KeyError:
-                    elb_bucket_name = ''
-                try:
-                    elb_bucket_prefix = elb_attributes['LoadBalancerAttributes']['AccessLog']['S3BucketPrefix']
-                except KeyError:
-                    elb_bucket_prefix = ''
+            elb_attributes = elb_client.describe_load_balancer_attributes(LoadBalancerName=elb_name)
+            try: 
+                if type(elb_attributes['LoadBalancerAttributes']['CrossZoneLoadBalancing']['Enabled']) == type(True):
+                    elb_cross_zone = 'True'
+            except KeyError:
+                elb_cross_zone = ''
+            try:
+                elb_access_logs = elb_attributes['LoadBalancerAttributes']['AccessLog']['Enabled']
+            except KeyError:
+                elb_access_logs = ''
+            try:
+                elb_bucket_name = elb_attributes['LoadBalancerAttributes']['AccessLog']['S3BucketName']
+            except KeyError:
+                elb_bucket_name = ''
+            try:
+                elb_bucket_prefix = elb_attributes['LoadBalancerAttributes']['AccessLog']['S3BucketPrefix']
+            except KeyError:
+                elb_bucket_prefix = ''
 
-                if elb_access_logs:
-                    elb_logs = elb_bucket_name + '/' + elb_bucket_prefix
-                    
+            if elb_access_logs:
+                elb_logs = elb_bucket_name + '/' + elb_bucket_prefix
+            
             for x in elb['ListenerDescriptions']:
                 temp = str(x['Listener']['LoadBalancerPort']) + ':' + str(x['Listener']['InstancePort']) + '\n'
                 listner = listner + temp
+            listner = listner.rstrip('\n')
 
             elb_details = elb_client.describe_tags(LoadBalancerNames=[elb_name,])
             for x in elb_details['TagDescriptions']:
@@ -63,8 +63,6 @@ class _Elb():
                     all_elb_data.append(elb_struct)
 
         if elb_data:
-            elb_details = elb_data
+            return elb_data
         else:
-            elb_details = all_elb_data
-        
-        return elb_details
+            return all_elb_data
