5e37fb9b6caf55a500b992bb33570a6aa986b903
dguyhasnoname <dguyhasnoname@gmail.com>
2021-05-17 23:36:11 +0530

    updated fields for elb

--- cluster/modules/get_elb.py
+++ cluster/modules/get_elb.py
@@ -7,9 +7,22 @@ class GetElb():
         elb_data, all_elb_data, elb_details = [], [], []
 
         for elb in elb_list['LoadBalancerDescriptions']:
-            elb_logs, elb_cross_zone, listner = '', '', ''
+            elb_logs, elb_cross_zone, listner, elb_dns, elb_dns_hosted_zone_id, elb_az, elb_subnets, elb_vpc, elb_sg, elb_creation_time = [''] * 10
             elb_name = elb['LoadBalancerName']
+            elb_dns = elb['DNSName']
+            elb_dns_hosted_zone_id = elb['CanonicalHostedZoneNameID']
+            elb_vpc = elb['VPCId']
+            elb_sg = elb['SourceSecurityGroup']['GroupName']
+            elb_creation_time = str(elb['CreatedTime'])
+
+            for az in elb['AvailabilityZones']:
+                elb_az += az + '\n'
+            elb_az = elb_az.rstrip('\n')
             
+            for sn in elb['Subnets']:
+                elb_subnets += sn + '\n'
+            elb_subnets = elb_subnets.rstrip('\n')
+
             elb_attributes = elb_client.describe_load_balancer_attributes(LoadBalancerName=elb_name)
             try: 
                 if type(elb_attributes['LoadBalancerAttributes']['CrossZoneLoadBalancing']['Enabled']) == type(True):
@@ -33,8 +46,14 @@ class GetElb():
                 elb_logs = elb_bucket_name + '/' + elb_bucket_prefix
             
             for x in elb['ListenerDescriptions']:
-                temp = str(x['Listener']['LoadBalancerPort']) + ':' + str(x['Listener']['InstancePort']) + '\n'
-                listner = listner + temp
+                try:
+                    temp = x['Listener']['Protocol'] + '/' +  \
+                            str(x['Listener']['LoadBalancerPort']) + ':' + \
+                            x['Listener']['InstanceProtocol'] + '/' + \
+                            str(x['Listener']['InstancePort']) + '\n'
+                    listner += temp
+                except:
+                    pass
             listner = listner.rstrip('\n')
 
             elb_details = elb_client.describe_tags(LoadBalancerNames=[elb_name,])
@@ -47,14 +66,19 @@ class GetElb():
                         namespace = tag['Value'].split('/')[0]
 
                 elb_struct = [elb_name, \
+                                elb_dns, \
                                 elb['Scheme'], \
                                 listner, \
                                 cluster_name, \
+                                elb_creation_time, \
                                 namespace, \
                                 elb_logs, \
+                                elb_vpc, \
+                                elb_sg, \
+                                elb_subnets, \
                                 elb_cross_zone, \
-                                elb['DNSName']\
-                                ]
+                                elb_az, \
+                                elb_dns_hosted_zone_id]
 
                 if cluster:
                     if cluster in cluster_name:
