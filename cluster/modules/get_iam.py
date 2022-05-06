a6f4477677be7b83b7aff96c80244f2240d8e5d6
ashish-dua <86287790+ashish-dua@users.noreply.github.com>
2022-05-06 12:58:58 +0530

    Update get_iam.py

--- cluster/modules/get_iam.py
+++ cluster/modules/get_iam.py
@@ -1,57 +1,18 @@
 class GetIam():
-    def get_iam_policy(session, cluster,iam_policy_arn):
-        global iam_client, iam_policy
+    def get_iam_policy_json(session, cluster,iam_policy_arn):
+        global iam_client, iam_policy, policyJson
         iam_client = session.client('iam')
         iam_policy = iam_client.Policy(iam_policy_arn)
-       for x in iam_list:
-            for iam in x['IAMRoles']:
-                capacity = str(iam['DesiredCapacity']) + '/' + str(asg['MinSize']) + '/' + str(asg['MaxSize'])
-                lb_names, az = '', ''
-                for i in asg['LoadBalancerNames']:
-                    lb_names = lb_names + i + '\n'
-                lb_names = lb_names.rstrip('\n')
-                for j in asg['AvailabilityZones']:
-                    az = az + j + '\n'
-                az = az.rstrip('\n')                    
-                if cluster:
-                    if cluster in asg['AutoScalingGroupName']:
-                        cluster_asg_list.append([asg['AutoScalingGroupName'], capacity, lb_names, az])
-                else:
-                    all_asg_list.append([asg['AutoScalingGroupName'], capacity, lb_names, az])
+        policyJson = iam_policy.default_version.document
+        return policyJson
 
-        if cluster_asg_list:
-            asg_data = cluster_asg_list
-        else:
-            asg_data = all_asg_list
-        return asg_data
 
-    def update_iam(session, cluster, update, logger):
-        # asg_data = GetIam.get_iam(session, cluster)
-
-        def update_iam_role(iamrole):
-            update_asg = asg_client.update_auto_scaling_group(AutoScalingGroupName=asg, MinSize=0, MaxSize=0, DesiredCapacity=0)
-            if update_asg['ResponseMetadata']['HTTPStatusCode'] == 200:
-                logger.info("asg {} updated successfully. HTTPStatusCode: {} ".\
-                    format(asg, update_asg['ResponseMetadata']['HTTPStatusCode']))
-            else:
-                logger.warning("asg {} update failed. HTTPStatusCode: {} ".\
-                    format(asg, update_asg['ResponseMetadata']['HTTPStatusCode']))   
-
-        for asg in asg_data:
-            if 'master' in update:
-                if 'master' in asg[0]:
-                    logger.info("Updating asg {}".format(asg[0]))
-                    update_auto_scaling_group(asg[0])
-            elif 'worker' in update or 'nodes' in update:
-                if 'cpu' in asg[0]:
-                    logger.info("Updating asg {}".format(asg[0]))
-                    update_auto_scaling_group(asg[0])
-            elif 'etcd' in update:
-                if 'etcd' in asg[0]:
-                    logger.info("Updating asg {}".format(asg[0]))
-                    update_auto_scaling_group(asg[0])
-            elif 'all' in update:
-                logger.info("Updating asg {}".format(asg[0]))
-                update_auto_scaling_group(asg[0])
-            else:
-                logger.warning("Invalid asg type input")
+        def update_iam_policy(iam_policy_arn):
+            policyJson['Statement'].update(newPolicyJson)
+            newiam_policy = client.create_policy_version(
+               PolicyArn= iam_policy_arn,
+               PolicyDocument= json.dumps(policyJson),
+               SetAsDefault= True
+            )
+            return newiam_policy
+            
